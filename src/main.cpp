73e0445feb3a067c4797387be4f4507f4ec768c4
fat83dotcom <fernando.quimic@gmail.com>
2023-10-29 15:45:10 -0300

    Novas funcionalidades.

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,24 @@
 #include <Arduino.h>
+#include <cstring> 
 #include "classesAndFunctions.h"
 
-#define TOPIC_SUBSCRIBE "ESP32_Receive_Information"
+#define TOPIC_PUBLISH_MAC "ESP32_MAC_BRAIN"
 #define TOPIC_PUBLISH_HUMIDITY "ESP32_Sensors_BME280_HUMI"
 #define TOPIC_PUBLISH_TEMPERATURE "ESP32_Sensors_BME280_TEMP"
 #define TOPIC_PUBLISH_PRESURE "ESP32_Sensors_BME280_PRESS"
-#define ID_MQTT "ESP32_MQTT_BRANISTORMTECH"
+
 
 #define led 2
 #define MSG_BUFFER_SIZE 50
 char msg[MSG_BUFFER_SIZE];
+String sMac = WiFi.macAddress();
+const char *mac = sMac.c_str();
 
 Mean temperatureMean;
 Mean pressureMean;
 Mean humidityMean;
-
-void printValues() {
-  static float count = 0;
-  static unsigned long currentTime = millis();
-  static unsigned long currentTimeLed = millis();
-  float temperature, humidity, pressure;
-
-  if ((millis() - currentTime) < 1){
-    temperature = 30.1F;
-    temperatureMean.sum(temperature);
-
-    pressure = 945.23F;
-    pressureMean.sum(pressure);
-
-    humidity = 45.4F;
-    humidityMean.sum(humidity);
-
-    count++;
-  }
-
-  if (count > 59){
-    Serial.print("Temperature = ");
-    Serial.print(temperatureMean.getMean(count));
-    Serial.println(" °C");
-    sprintf(msg, "%.2f", temperatureMean.getMean(count));
-    temperatureMean.resetSum();
-    MQTT.publish(TOPIC_PUBLISH_TEMPERATURE, msg);
-
-    Serial.print("Pressure = ");
-    Serial.print(pressureMean.getMean(count));
-    Serial.println(" hPa");
-    sprintf(msg, "%.2f", pressureMean.getMean(count));
-    pressureMean.resetSum();
-    MQTT.publish(TOPIC_PUBLISH_PRESURE, msg);
-
-    Serial.print("Humidity = ");
-    Serial.print(humidityMean.getMean(count));
-    Serial.println(" %");
-    sprintf(msg, "%.2f", humidityMean.getMean(count));
-    MQTT.publish(TOPIC_PUBLISH_HUMIDITY, msg);
-    humidityMean.resetSum();
-    count = 0;
-
-    Serial.println();
-  }
-  
-  if ((millis() - currentTime) > 999){
-    currentTime = millis();
-  }
-
-  if ((millis() - currentTimeLed) < 500) {
-    digitalWrite(led, 1);
-  }
-  else {
-    digitalWrite(led, 0);
-  }
-  if ((millis() - currentTimeLed) > 1000){
-    currentTimeLed = millis();
-  }
-}
+Counter count;
+Counter getDataSensorCounter;
 
 void setup() {
   initSerial();
@@ -83,9 +28,88 @@ void setup() {
   pinMode(led, OUTPUT);
 }
 
+void ledBlink() {
+  static unsigned long currentTimeLed = millis();
+  if ((millis() - currentTimeLed) < 500) {
+    digitalWrite(led, 1);
+  }
+
+  else {
+    digitalWrite(led, 0);
+  }
+
+  if ((millis() - currentTimeLed) > 1000){
+    currentTimeLed = millis();
+  }
+}
+
+void getSensorData() {
+  float temperature, humidity, pressure;
+  if (int(getDataSensorCounter.getCounter()) % 7000 == 0) {
+    temperature = 30.1F;
+    temperatureMean.sum(temperature);
+
+    pressure = 945.23F;
+    pressureMean.sum(pressure);
+
+    humidity = 45.4F;
+    humidityMean.sum(humidity);
+
+    count.increaseCounter();
+  }
+
+  getDataSensorCounter.increaseCounter();
+  ledBlink();
+}
+
+void printValues() {
+  Serial.println(count.getCounter());
+
+  MQTT.publish(TOPIC_PUBLISH_MAC, mac);
+  Serial.println(mac);
+
+  Serial.print("Temperature = ");
+  Serial.print(temperatureMean.getMean(count.getCounter()));
+  Serial.println(" °C");
+  sprintf(msg, "%.2f", temperatureMean.getMean(count.getCounter()));
+  temperatureMean.resetSum();
+  MQTT.publish(TOPIC_PUBLISH_TEMPERATURE, msg);
+
+  Serial.print("Pressure = ");
+  Serial.print(pressureMean.getMean(count.getCounter()));
+  Serial.println(" hPa");
+  sprintf(msg, "%.2f", pressureMean.getMean(count.getCounter()));
+  pressureMean.resetSum();
+  MQTT.publish(TOPIC_PUBLISH_PRESURE, msg);
+
+  Serial.print("Humidity = ");
+  Serial.print(humidityMean.getMean(count.getCounter()));
+  Serial.println(" %");
+  sprintf(msg, "%.2f", humidityMean.getMean(count.getCounter()));
+  humidityMean.resetSum();
+  MQTT.publish(TOPIC_PUBLISH_HUMIDITY, msg);
+  
+  count.resetCounter();
+  getDataSensorCounter.resetCounter();
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  String msg;
+
+  for(int i = 0; i < length; i++){
+    char c = (char)payload[i];
+    msg += c;
+  }
+  if (msg == "return") {
+    const char *cMsg = msg.c_str();
+    printValues();
+    Serial.println(cMsg);
+  }
+}
+
 void loop() { 
   checkConnectionsWifiMqtt();
-  printValues();
-  /* keep-alive da comunicação com broker MQTT */    
+  MQTT.setCallback(mqttCallback);
+  getSensorData();
   MQTT.loop();
 }
